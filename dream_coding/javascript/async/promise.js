c878c8a5e4287bdd776b5cdb60dcae9c7a7b8636
Kim Dong-hyeon <66898812+ms0130k@users.noreply.github.com>
2021-02-03 08:58:50 +0900

    promise

---
+++ dream_coding/javascript/async/promise.js
@@ -0,0 +1,42 @@
+'use strict';
+
+// Promise is a JavaScript object for asynchronous operation.
+// State: pending -> fulfilled or rejected
+// Producer vs Consumer
+
+// 1. Producer
+// when new Promise is created, the executor runs automatically.
+const promise = new Promise((resolve, reject) => {
+	// doding some heavy work (network, read files)
+	console.log('doning something...');
+	setTimeout(() => {
+		// resolve('shock');
+		reject(new Error('no network'));
+	}, 1000);
+});
+
+promise
+	.then((value) => {
+		console.log(value);
+	})
+	.catch((error) => {
+		console.log(error);
+	})
+	.finally(() => {
+		console.log('finally');
+	});
+
+// 3. Promise chaining
+const fetchNumber = new Promise((resolve, reject) => {
+	setTimeout(() => resolve(1), 1000);
+});
+
+fetchNumber
+	.then((num) => num * 2)
+	.then((num) => num * 3)
+	.then((num) => {
+		return new Promise((resolve, reject) => {
+			setTimeout(() => resolve(num - 1), 1000);
+		});
+	})
+	.then((num) => console.log(num));
