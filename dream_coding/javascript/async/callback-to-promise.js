e7ff83496733b864c60c632599e748424d2e0a93
Kim Dong-hyeon <66898812+ms0130k@users.noreply.github.com>
2021-02-26 08:26:29 +0900

    update

--- dream_coding/javascript/async/callback-to-promise.js
+++ dream_coding/javascript/async/callback-to-promise.js
@@ -2,26 +2,36 @@
 
 // Callback Hell example
 class UserStorage {
-	loginUser(id, password, onSuccess, onError) {
-		setTimeout(() => {
-			if (
-				(id === 'shock' && password === 'dream') ||
-				(id === 'coder' && password === 'academy')
-			) {
-				onSuccess(id);
-			} else {
-				onError(new Error('not found'));
-			}
-		}, 1000);
+	loginUser(id, password) {
+		return new Promise((resolve, reject) => {
+			setTimeout(() => {
+				if (
+					(id === 'shock' && password === 'dream') ||
+					(id === 'coder' && password === 'academy')
+				) {
+					resolve(id);
+				} else {
+					reject(new Error('not found'));
+				}
+			}, 1000);
+		});
 	}
 
-	getRoles(user, onSuccess, onError) {
-		setTimeout(() => {
-			if (user === 'shock') {
-				onSuccess({ name: 'shock', role: 'admin' });
-			} else {
-				onError(new Error('no access'));
-			}
-		}, 2000);
+	getRoles(user) {
+		return new Promise((resolve, reject) => {
+			setTimeout(() => {
+				if (user === 'shock') {
+					resolve({ name: 'shock', role: 'admin' });
+				} else {
+					reject(new Error('no access'));
+				}
+			}, 2000);
+		});
 	}
 }
+
+new UserStorage()
+	.loginUser('coder', 'academy')
+	.then(new UserStorage().getRoles)
+	.then(console.log)
+	.catch(console.log);
